e5f5992dbe234e2f1252a36a4d1a8aa53832046d
Maximiliano Stein <maxi_stein23@hotmail.com>
2024-03-25 18:40:03 -0300

    se corrije formateo y se agrega la clase getbookdto

--- src/dto/publisher.dto.ts
+++ src/dto/publisher.dto.ts
@@ -1,13 +1,20 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, isNotEmpty } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 export class CreatePublisherDto {
-  @ApiProperty({description: 'Publisher\'s name'})
+  @ApiProperty({ description: "Publisher's name" })
   @IsNotEmpty()
   readonly name: string;
-  @ApiProperty({description: 'Publisher\'s address. Please specify address, postal, city and country'})
+  @ApiProperty({
+    description:
+      "Publisher's address. Please specify address, postal, city and country",
+  })
   @IsNotEmpty()
   readonly address: string;
-  @ApiProperty({description: 'Publisher\'s CUIT. Consists of 2 digits, 1 colon, 8 digits, 1 colon and 1 addition digit.', example: '23-34460293-2'})
+  @ApiProperty({
+    description:
+      "Publisher's CUIT. Consists of 2 digits, 1 colon, 8 digits, 1 colon and 1 addition digit.",
+    example: '23-34460293-2',
+  })
   @IsNotEmpty()
   readonly cuit: string;
 }
